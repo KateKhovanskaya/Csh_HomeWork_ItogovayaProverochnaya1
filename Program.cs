1b407c02aa1cf9736a86ae91f6563a2d12060163
Kate <KhovanskayaK@yandex.ru>
2023-02-13 23:40:54 +0300

    задача решена при заданном на старте массиве

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr={"hello", "2", "world", ":-)"};
+int rezCount = LengthRezaltArr(arr);
+Console.Write($"[{String.Join(",", FillRezultArr(rezCount))}]");
+//----------------Методы----------------
+//Определение длинв результативного массива
+int LengthRezaltArr(string[]arr){
+    int rezCount=0;
+    for(int i=0;i<arr.Length;i++){
+        if(arr[i].Length<=3){
+         rezCount++;
+         }
+    }
+    return rezCount;
+}
+
+//Заполнение результативного массива
+string[] FillRezultArr(int rezCount){
+    string[] rezArr=new string[rezCount];
+    int j=0;
+    for(int i=0;i<arr.Length;i++){
+        if(arr[i].Length<=3){
+            rezArr[j]=arr[i];
+            j++;
+        }
+    }
+    return rezArr;
+}
